5e2db450d32d259aaaad4b23e780b23d06341253
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2010-06-19 13:32:16 +0100

    first version of pyjamas pyjs site

---
+++ PageLoader.py
@@ -0,0 +1,42 @@
+
+class PageListLoader:
+    def __init__(self, panel):
+        self.panel = panel
+
+    def onCompletion(self, text):
+        """ pages.txt is in the format:
+               title:filename.html
+               title2:filename2.html
+        """
+        res = []
+        for l in text.split('\n'):
+            if not l:
+                continue
+            l = l.split(':')
+            if len(l) != 2:
+                continue
+            res.append([l[0].strip(), l[1].strip()])
+        self.panel.loadPages(res)
+
+    def onError(self, text, code):
+        self.panel.onError(text, code)
+
+    def onTimeout(self, text):
+        self.panel.onTimeout(text)
+
+
+class PageLoader:
+    def __init__(self, panel, title):
+        self.panel = panel
+        self.title = title
+
+    def onCompletion(self, text):
+        self.panel.createPage(title, text)
+
+    def onError(self, text, code):
+        self.panel.onError(text, code)
+
+    def onTimeout(self, text):
+        self.panel.onTimeout(text)
+
+
---
+++ index.py
@@ -0,0 +1,89 @@
+import pyjd # dummy in pyjs
+
+from pyjamas.ui.TabBar import TabBar
+from pyjamas.ui.TabPanel import TabPanel
+from pyjamas.ui import HasAlignment
+from pyjamas.ui.Image import Image
+from pyjamas.ui.VerticalPanel import VerticalPanel
+from pyjamas.ui.RootPanel import RootPanel
+from pyjamas.ui.HorizontalPanel import HorizontalPanel
+from pyjamas.ui.HTML import HTML
+from pyjamas.ui.Composite import Composite
+#from pyjamas.ui import DecoratorPanel
+from pyjamas.ui import MouseListener
+from pyjamas.ui import Event
+from pyjamas import Window
+from pyjamas.ui.DecoratorPanel import DecoratedTabPanel, DecoratorPanel
+from pyjamas.ui.DecoratorPanel import DecoratorTitledPanel
+from pyjamas.HTTPRequest import HTTPRequest
+from PageLoader import PageListLoader, PageLoader
+
+
+#class PrettyTab(DecoratorPanel):
+class PrettyTab(Composite):
+
+    def __init__(self, text, imageUrl):
+
+        DecoratorPanel.__init__(self, DecoratorPanel.DECORATE_ALL)
+
+        p = HorizontalPanel()
+        p.setSpacing(3)
+        self.img = Image(imageUrl)
+        self.txt = HTML(text)
+        p.add(self.img)
+        p.add(self.txt)
+
+        self.add(p)
+
+    def addClickListener(self, listener):
+
+        self.img.addClickListener(listener)
+        self.txt.addClickListener(listener)
+
+class Tabs:
+
+    def onModuleLoad(self):
+
+        self.fTabs = DecoratedTabPanel(Size=("600px", "100%"))
+        #self.fTabs.add(HTML("shouldn't be here!"), None)
+        #self.fTabs.add(HTML("This is a Test.<br />Tab should be on right"),
+        #               "Test")
+        #self.fTabs.selectTab(0)
+
+        dp = DecoratorTitledPanel("Tabs", "bluetitle", "bluetitleicon",
+                      ["bluetop", "bluetop2", "bluemiddle", "bluebottom"])
+        dp.add(self.fTabs)
+        RootPanel().add(dp)
+
+        self.loadPageList()
+
+    def createPage(self, title, text):
+        self.fTabs.add(HTML(text), title, True)
+
+    def createImage(self, imageUrl):
+        image = Image(imageUrl)
+        image.setStyleName("ks-images-Image")
+        
+        p = VerticalPanel()
+        p.setHorizontalAlignment(HasAlignment.ALIGN_CENTER)
+        p.setVerticalAlignment(HasAlignment.ALIGN_MIDDLE)
+        p.add(image)
+
+        return p
+
+    def loadPageList(self):
+        HTTPRequest().asyncGet("contents.txt", PageListLoader(self))
+
+    def loadPages(self, pages):
+        for l in pages:
+            name = l[0]
+            desc = l[1]
+            HTTPRequest().asyncGet(desc, PageLoader(self, name))
+
+
+if __name__ == '__main__':
+    pyjd.setup("./public/index.html")
+    app = Tabs()
+    app.onModuleLoad()
+    pyjd.run()
+
